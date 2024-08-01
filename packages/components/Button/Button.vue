e2b6cecb08c77d5e7ba687b4a0333bdfcd5bc6f0
10646 <741838362@qq.com>
2024-08-01 16:41:56 +0800

    样式

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,29 +1,20 @@
 <script setup lang="ts">
 defineOptions({
-    name: 'LButton'
+    name: 'LeeButton'
 })
 
 </script>
 
 <template>
-    <button class="l-button">
+    <button class="lee-button">
         <slot></slot>
     </button>
 </template>
 
-<style lang="scss" scoped>
-.l-button {
+<style  scoped>
+.lee-button {
     background-color: pink;
     display: inline-block;
-    padding: 6px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1.42857143;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    -ms-touch-action: manipulation;
-    touch-action: manipulation;
-    cursor: pointer;
+    color: #fff;
     }
+</style>
